02570eae2a3421e6aff6a26efe674344d946329f
whatsappterabot-arch <whatsapp.terabot@gmail.com>
2026-07-23 01:45:31 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js
+export const CONFIG = {
+    // Webhook exclusivo para o fluxo de autenticação (index.html)
+    LOGIN_URL: 'https://awkwardgiantpanda-n8n.cloudfy.live/webhook-test/f9f1ae2e-018c-4d02-b121-b346d641bbf1',
+
+    // Webhook para todas as outras atividades (CRUD, listagem, etc)
+    API_URL: 'https://awkwardgiantpanda-n8n.cloudfy.live/webhook-test/df72b1bc-ce3b-456b-a0b6-dfa8ecc34408',
+
+    APP_NAME: 'MidiaBot'
+};
